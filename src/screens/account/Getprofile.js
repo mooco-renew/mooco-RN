c9a31577e414edc2bac96159d728123d19562fcb
eunji <bian87@dgu.ac.kr>
2024-03-05 16:05:00 +0900

    ADD: camera option bar css 생성

--- src/screens/account/Getprofile.js
+++ src/screens/account/Getprofile.js
@@ -1,47 +1,19 @@
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
 import { View, Text, TouchableOpacity, Pressable, StyleSheet, TextInput, Image } from 'react-native';
+import ProfileImage from '../../components/getProfile/profileImage';
 
 
 // test용 스크린
 export default function GetProfile() {
     const navigation = useNavigation();
     const [nickname, setNickname] = useState("");
-
-    // 이미지 권한 요청을 위한 hooks
-    const [status, requestPermisson] = ImagePicker.useMediaLibraryPermissions();
     const [imageUrl, setImageUrl] = useState(null); // 기본 이미지
     const [isUploaded, setIsUploaded] = useState(false);
 
-    const uploadImage = async () => {
-        // 권한 확인 코드 : 권한 없으면 물어보고, 승인하지 않으면 함수 종료
-        if (!status?.granted) {
-            const permission = await requestPermisson();
-            if(!permission.granted) {
-                return null;
-            }
-        }
-        // 이미지 업로드 기능
-        const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: false,
-            quality: 1,
-            aspect: [1, 1],
-        });
-        if(result.canceled) {
-            return null; // 이미지 업로드를 취소한 경우
-        }
-        // 이미지 업로드 결과 및 이미지 경로 업데이트
-        setImageUrl(result.assets[0].uri);
-        setIsUploaded(true);
-    }
-
     return (
       <View style={styles.container}>
-        <Pressable onPress={uploadImage}>
-        <Image style={styles.image} source={isUploaded ? { uri: imageUrl } : require('../../assets/images/getProfile/pre-image.png')} />
-        </Pressable>
+        <ProfileImage />
         <View style={styles.inputbox}>
         <TextInput 
         value={nickname}
