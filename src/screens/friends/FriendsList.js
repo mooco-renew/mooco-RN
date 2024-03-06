a06c5cd2322a89773f67c6704ac442282f897671
eunji <bian87@dgu.ac.kr>
2024-03-06 11:37:10 +0900

    STYLE: 친구추가 input bar style 변경

--- src/screens/friends/FriendsList.js
+++ src/screens/friends/FriendsList.js
@@ -4,6 +4,7 @@ import { View, Text, TouchableOpacity, StyleSheet, TextInput, ScrollView } from 
 import DeleteFriend from '../../components/friends/DeleteFriend';
 import CustomSwitch from '../../components/switch/CustomSwitch';
 import DeleteFriendAlert from '../../components/alert/deletefriendalert';
+import SearchSvg from '../../assets/images/friends/search';
 
 // test용 스크린
 export default function FriendsList() {
@@ -19,12 +20,17 @@ export default function FriendsList() {
     return (
       <View style={styles.container}>
         {view ? ( <DeleteFriendAlert setView={setView}/>) : ( <></> )}
+        <View style={styles.inputcontainer}>
+          <View style={styles.search}>
+        <SearchSvg />
+        </View>
         <TextInput 
         value={search}
         style={styles.input}
         onChangeText={setSearch}
         placeholder='추가하고 싶은 친구의 아이디를 검색해보세요!'
         placeholderTextColor={'rgba(0,0,0,0.5)'}/>
+        </View>
         <View style={styles.subcontainer}>
             <Text style={styles.label}>친구 목록</Text>
              <ScrollView style={styles.scrollbox} contentContainerStyle={{alignItems: 'center'}}>
@@ -49,7 +55,18 @@ export default function FriendsList() {
       width: '100%',
       height: '100%',
       alignItems: 'center',
-      backgroundColor: '#151515',
+      backgroundColor: '#000000',
+    },
+    inputcontainer: {
+      position: 'relative',
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    search: {
+      top: 15,
+      left: 35,
+      zIndex: 10,
     },
     subcontainer: {
         width: '100%',
@@ -80,8 +97,8 @@ export default function FriendsList() {
       backgroundColor: '#ffffff',
       fontSize: 14,
       borderRadius: 10,
-      paddingHorizontal: 20,
-      paddingVertical: 7,
+      paddingHorizontal: 43,
+      paddingVertical: 14,
       marginTop: 30,
     },
     scrollbox: {
