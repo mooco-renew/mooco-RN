6887ace391550bea867b1df54ef03a947faa06b7
eunji <bian87@dgu.ac.kr>
2024-03-15 00:22:57 +0900

    ADD: app.js 경로 추가

--- App.js
+++ App.js
@@ -18,6 +18,8 @@ import DailyUpload from "./src/screens/daily/DailyUpload";
 import AppBar from "./src/components/navigation/bar/AppBar";
 import { NativeBaseProvider } from "native-base";
 import HomeTab from "./src/screens/homeTab/homeTab";
+import Account from "./src/screens/account/Account";
+import Login from "./src/screens/login/Login";
 // rn navigatior로 stack 생성, rn은 stack으로 사용자의 이동을 확인한다.
 const Stack = createNativeStackNavigator();
 
@@ -37,10 +39,25 @@ function App() {
             component={OnBoarding}
             options={{ headerShown: false }}
           />
+           <Stack.Screen
+            name="Account"
+            component={Account}
+            options={{ ...backbtncolorbar, title: "회원가입" }}
+          />
+          <Stack.Screen
+            name="Agreement"
+            component={Agreement}
+            options={{ ...backbtncolorbar, title: "이용 약관 동의서" }}
+          />
+           <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ ...backbtncolorbar, title: "로그인" }}
+          />
           <Stack.Screen
             name="GetProfile"
             component={GetProfile}
-            options={{ ...backbtncolorbar, title: "프로필 편집" }}
+            options={{ ...backbtncolorbar, title: "프로필 등록" }}
           />
           <Stack.Screen name="KakaoLogin" component={KakaoLoginScreen} />
           <Stack.Screen
