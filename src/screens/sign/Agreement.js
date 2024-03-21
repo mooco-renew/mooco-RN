8b8ba0a9e55a14426657b671d991fff9383e01f0
eunji <bian87@dgu.ac.kr>
2024-03-22 02:37:57 +0900

    REFACTOR: 이용동의 terms 추가

--- src/screens/sign/Agreement.js
+++ src/screens/sign/Agreement.js
@@ -2,97 +2,85 @@ import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import Checkbox from 'expo-checkbox';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { setNewTrueArray } from '../../util/array/newTrueArray';
+import { setNewFalseArray } from '../../util/array/newFalseArray';
+import { allFalse, firstFalseOnly, firstTrueOnly, lastFalseOnly } from '../../util/auth/authStep';
+import DetailTerms from '../../components/sign/detailTerms';
 
-export default function Agreement({ route }) {
+export default function Agreement() {
   const navigation = useNavigation();
 
   const [allchecked, setAllchecked] = useState(false);
-    const [firstchecked, setFirstChecked] = useState(false);
-    const [secondchecked, setSecondChecked] = useState(false);
-    const [thirdchecked, setThirdChecked] = useState(false);
-    const [fourchecked, setFourChecked] = useState(false);
-    const [isAvail, setIsAvail] = useState(false);
-
-    // navigation으로 보낸 params 받기
-    const { id, pw } = route.params;
-
-    const AllChecked = () => {
-      if(firstchecked == true && secondchecked == true && thirdchecked == true && fourchecked == true) {
-        setAllchecked(false);
-        setFirstChecked(false);
-        setSecondChecked(false);
-        setThirdChecked(false);
-        setFourChecked(false);
-      } else {
-        setAllchecked(true);
-        setFirstChecked(true);
-        setSecondChecked(true);
-        setThirdChecked(true);
-        setFourChecked(true);
-      }
-    }
+    const [firstchecked, setFirstChecked] = useState(false); // 이용약관 동의서
+    const [secondchecked, setSecondChecked] = useState(false); // 개인정보 보호 동의서
+    const [isAvail, setIsAvail] = useState(false); // 전체 동의 하였는지 판단
+    const [isShow, setShow] = useState([false, false]);
 
     // 모든 선택지를 true로 한다면, allchecked도 true로 변경한다. ++ 유효성 검사
     useEffect(() => {
-      if(firstchecked == true && secondchecked == true && thirdchecked == true && fourchecked == true) {
+      if(firstchecked == true && secondchecked == true) {
         setAllchecked(true);
         setIsAvail(true);
-      } else {
-        setAllchecked(false);
-        setIsAvail(false);
+      } 
+    }, [firstchecked, secondchecked]);
+
+    const isShowFunc = (index) => {
+      if(index == 0) { 
+        let newArray = [true, false];
+        setShow(newArray);
+       }
+      else if(index == 1) { 
+        let newArray = [false, true];
+        setShow(newArray);
       }
-    }, [firstchecked, secondchecked, thirdchecked, fourchecked, allchecked]);
+      else if(index == 4) {
+        let newArray = [false, false];
+        setShow(newArray);
+      }
+    }
 
   return (
     <View style={styles.container}>
-        <View style={styles.firstcheckcontainer}>
-    <Checkbox
-          style={styles.checkbox}
-          value={allchecked}
-          onValueChange={() => AllChecked()}
-          color={allchecked ? '#000000' : 'rgba(255,255,255,0.3)'}
-        />
-    <Text style={styles.checktext}>전체 항목에 동의합니다.</Text>
-    </View>
-    <View style={styles.checkcontainer}>
-    <Checkbox
-          style={styles.checkbox}
-          value={firstchecked}
-          onValueChange={setFirstChecked}
-          color={allchecked ? '#000000' : 'rgba(255,255,255,0.3)'}
-        />
-    <Text style={styles.checktext}>약관1)</Text>
-    </View>
-    <View style={styles.checkcontainer}>
-    <Checkbox
-          style={styles.checkbox}
-          value={secondchecked}
-          onValueChange={setSecondChecked}
-          color={allchecked ? '#000000' : 'rgba(255,255,255,0.3)'}
-        />
-    <Text style={styles.checktext}>약관2)</Text>
-    </View>
-    <View style={styles.checkcontainer}>
-    <Checkbox
-          style={styles.checkbox}
-          value={thirdchecked}
-          onValueChange={setThirdChecked}
-          color={allchecked ? '#000000' : 'rgba(255,255,255,0.3)'}
-        />
-    <Text style={styles.checktext}>약관3)</Text>
-    </View>
-    <View style={styles.checkcontainer}>
-    <Checkbox
-          style={styles.checkbox}
-          value={fourchecked}
-          onValueChange={setFourChecked}
-          color={allchecked ? '#000000' : 'rgba(255,255,255,0.3)'}
-        />
-    <Text style={styles.checktext}>약관4)</Text>
-    </View>
-    <TouchableOpacity style={[styles.button, !isAvail && styles.buttondisable]} onPress={() => navigation.navigate('Account', { _checked: true })} disabled={!isAvail}>
-        <Text style={styles.buttontext}>완료</Text>
-    </TouchableOpacity>
+      {allFalse(isShow) && (
+         <View style={styles.container}>
+           <View style={styles.firstcheckcontainer}>
+       <Checkbox
+             style={styles.checkbox}
+             value={allchecked}
+             onChange={setAllchecked}
+             color={allchecked ? '#000000' : 'rgba(255,255,255,0.3)'}
+           />
+       <Text style={styles.checktext}>전체 항목에 동의합니다.</Text>
+       </View>
+       <View style={styles.checkcontainer}>
+          <View>
+          <TouchableOpacity style={styles.checkbutton} onPress={() => isShowFunc(0)}>
+       <Checkbox
+             style={styles.checkbox}
+             value={firstchecked}
+             color={allchecked ? '#000000' : 'rgba(255,255,255,0.3)'}
+           />
+       <Text style={styles.checktext}>이용약관 동의서</Text>
+       </TouchableOpacity>
+       </View>
+       </View>
+       <View style={styles.checkcontainer}>
+       <TouchableOpacity style={styles.checkbutton} onPress={() => isShowFunc(1)}>
+       <Checkbox
+             style={styles.checkbox}
+             value={secondchecked}
+             color={allchecked ? '#000000' : 'rgba(255,255,255,0.3)'}
+           />
+       <Text style={styles.checktext}>개인정보 이용 동의서</Text>
+       </TouchableOpacity>
+       </View>
+       <TouchableOpacity style={[styles.button, !isAvail && styles.buttondisable]} onPress={() => navigation.navigate('Account', { _checked: true })} disabled={!isAvail}>
+           <Text style={styles.buttontext}>완료</Text>
+       </TouchableOpacity>
+       </View>
+      )}
+       {firstTrueOnly(isShow) && <DetailTerms index={0} setCheck={setFirstChecked} isShowFunc={isShowFunc}/>}
+       {firstFalseOnly(isShow) && <DetailTerms index={1} setCheck={setSecondChecked} isShowFunc={isShowFunc}/>}
     </View>
   )
 }
@@ -104,6 +92,10 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: '#000000',
     },
+    checkbutton: {
+      display: 'flex',
+      flexDirection: 'row',
+    },
     button: {
       backgroundColor: '#151515',
       alignItems: 'center',
@@ -111,7 +103,7 @@ const styles = StyleSheet.create({
       width: '90%', 
       paddingVertical: 18, // 상하 패딩 
       borderRadius: 10,
-      marginTop: 40,
+      marginTop: 20,
     },
     buttondisable: {
       backgroundColor: 'rgba(217, 217, 217, 0.5)',
