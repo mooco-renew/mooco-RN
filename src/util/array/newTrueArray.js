1b0ff6be180168db8bf88e88da1e7b626525e2b0
eunji <bian87@dgu.ac.kr>
2024-03-22 02:37:26 +0900

    ADD: 배열 값 true, false로 변경 함수 추가

---
+++ src/util/array/newTrueArray.js
@@ -0,0 +1,8 @@
+    // 배열 true로 변환
+    export const setNewTrueArray = (setAuth, index) => {
+        setAuth((prevAuth) => {
+            const newAuth = [...prevAuth]; // 배열 복사
+            newAuth[index] = true; // 특정 인덱스의 값을 true로 설정
+            return newAuth; // 업데이트된 배열 반환
+        });
+    };
